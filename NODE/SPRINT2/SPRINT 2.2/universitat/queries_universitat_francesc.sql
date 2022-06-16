db7f10a0b1b20f2969572adbf898f7c402de73e0
unknown <francescbages@gmail.com>
2022-06-16 09:35:32 +0200

    Queries entrega sense comentaris

--- NODE/SPRINT2/SPRINT 2.2/universitat/queries_universitat_francesc.sql
+++ NODE/SPRINT2/SPRINT 2.2/universitat/queries_universitat_francesc.sql
@@ -1,81 +1,27 @@
 USE universidad;
--- 1
--- SELECT apellido1, apellido2, nombre FROM persona WHERE tipo = 'alumno' ORDER BY apellido1, apellido2, nombre ASC;
-
--- 2
--- SELECT nombre, apellido1, apellido2 FROM persona WHERE tipo = 'alumno' AND telefono IS NULL;
-
--- 3
--- SELECT * FROM persona WHERE tipo = 'alumno' AND YEAR(fecha_nacimiento) = '1999';
-
--- 4
--- SELECT * FROM persona WHERE tipo = 'profesor' AND telefono IS NULL AND nif LIKE '%K' ;
-
--- 5
--- SELECT * FROM asignatura WHERE cuatrimestre = 1 AND curso = 3 AND id_grado = 7;
-
--- 6
- -- SELECT  persona.apellido1, persona.apellido2, persona.nombre, departamento.nombre FROM persona LEFT JOIN profesor ON persona.id = profesor.id_profesor INNER JOIN departamento ON departamento.id = profesor.id_departamento ORDER BY persona.apellido1, persona.apellido2, persona.nombre ASC;
-
--- 7
- -- SELECT asignatura.nombre, curso_escolar.anyo_inicio, curso_escolar.anyo_fin FROM asignatura INNER JOIN alumno_se_matricula_asignatura ON alumno_se_matricula_asignatura.id_asignatura = asignatura.id INNER JOIN persona ON alumno_se_matricula_asignatura.id_alumno = persona.id INNER JOIN curso_escolar ON curso_escolar.id=alumno_se_matricula_asignatura.id_curso_escolar WHERE nif = '26902806M';
- 
- -- 8
- -- SELECT DISTINCT departamento.nombre AS 'Departamento' FROM persona INNER JOIN profesor ON profesor.id_profesor = persona.id INNER JOIN departamento ON departamento.id = profesor.id_departamento INNER JOIN asignatura ON asignatura.id_profesor = profesor.id_profesor INNER JOIN grado ON grado.id = asignatura.id_grado WHERE persona.tipo = 'profesor' AND grado.nombre = 'Grado en Ingeniería Informática (Plan 2015)';
- 
- -- 9
- -- SELECT DISTINCT persona.nombre, persona.apellido1, persona.apellido2 FROM persona LEFT JOIN alumno_se_matricula_asignatura ON persona.id = alumno_se_matricula_asignatura.id_alumno LEFT JOIN curso_escolar ON curso_escolar.id = alumno_se_matricula_asignatura.id_curso_escolar WHERE curso_escolar.anyo_inicio = '2018';
- 
- -- SECCIO LEFT JOIN I RIGHT JOIN
- -- 1
- -- SELECT departamento.nombre AS 'DEPARTAMENTO', persona.nombre, persona.apellido1, persona.apellido2 FROM persona LEFT JOIN profesor ON profesor.id_profesor = persona.id LEFT JOIN departamento ON departamento.id = profesor.id_departamento WHERE persona.tipo = 'profesor' ORDER BY departamento.nombre ASC, persona.apellido1 ASC, persona.apellido2 ASC, persona.nombre ASC;
- 
- -- 2
--- SELECT persona.nombre, persona.apellido1, persona.apellido2 FROM persona LEFT JOIN profesor ON profesor.id_profesor = persona.id LEFT JOIN departamento ON departamento.id = profesor.id_departamento WHERE profesor.id_departamento IS NULL AND persona.tipo = 'profesor';
- 
- -- 3
- -- SELECT departamento.nombre AS 'DEPARTAMENTO' FROM departamento LEFT JOIN profesor ON departamento.id = profesor.id_departamento WHERE profesor.id_departamento IS NULL;
- 
- -- 4
- -- SELECT * FROM persona LEFT JOIN profesor ON profesor.id_profesor = persona.id LEFT JOIN asignatura ON asignatura.id_profesor = profesor.id_profesor WHERE asignatura.id_profesor IS NULL AND persona.tipo = 'profesor';
- 
- -- 5
--- SELECT asignatura.nombre FROM asignatura LEFT JOIN profesor ON profesor.id_profesor = asignatura.id_profesor WHERE profesor.id_profesor IS NULL;
-
--- 6
--- SELECT DISTINCT departamento.nombre FROM departamento LEFT JOIN profesor ON profesor.id_departamento = departamento.id LEFT JOIN asignatura ON asignatura.id_profesor = profesor.id_profesor LEFT JOIN alumno_se_matricula_asignatura ON alumno_se_matricula_asignatura.id_asignatura = asignatura.id LEFT JOIN curso_escolar ON curso_escolar.id = alumno_se_matricula_asignatura.id_curso_escolar WHERE curso_escolar.anyo_inicio IS NULL;
-
--- Consultes resum
--- 1
--- SELECT COUNT(persona.id) FROM persona WHERE persona.tipo = 'alumno';
-
--- 2
--- SELECT COUNT(persona.id) FROM persona WHERE persona.tipo = 'alumno' AND YEAR(persona.fecha_nacimiento)='1999';
-
--- 3
--- SELECT departamento.nombre, COUNT(profesor.id_profesor) FROM departamento INNER JOIN profesor ON profesor.id_departamento = departamento.id GROUP BY departamento.id	 ORDER BY COUNT(profesor.id_profesor) DESC;
-
--- 4
--- SELECT departamento.nombre, COUNT(profesor.id_profesor) FROM departamento LEFT JOIN profesor ON profesor.id_departamento = departamento.id GROUP BY departamento.id ORDER BY COUNT(profesor.id_profesor) ASC; 
-
--- 5
---  SELECT grado.nombre, COUNT(asignatura.id) FROM grado LEFT JOIN asignatura ON asignatura.id_grado = grado.id GROUP BY grado.id ORDER BY COUNT(asignatura.id) DESC;
-
--- 6
--- SELECT grado.nombre, COUNT(asignatura.id) FROM grado INNER JOIN asignatura ON asignatura.id_grado = grado.id  GROUP BY grado.id HAVING COUNT(asignatura.id)>40  ORDER BY COUNT(asignatura.id) DESC;
-
--- 7
--- SELECT grado.nombre, asignatura.tipo, COUNT(asignatura.creditos) FROM grado LEFT JOIN asignatura ON asignatura.id_grado = grado.id GROUP BY grado.id, asignatura.tipo;
-
--- 8
--- SELECT curso_escolar.anyo_inicio, COUNT(persona.id) FROM curso_escolar INNER JOIN alumno_se_matricula_asignatura ON alumno_se_matricula_asignatura.id_curso_escolar = curso_escolar.id INNER JOIN persona ON persona.id	= alumno_se_matricula_asignatura.id_alumno WHERE persona.tipo = 'alumno' GROUP BY curso_escolar.id;
-
--- 9
--- SELECT persona.id, persona.nombre, persona.apellido1, persona.apellido2, COUNT(asignatura.id) FROM persona INNER JOIN profesor ON profesor.id_profesor = persona.id INNER JOIN asignatura ON asignatura.id_profesor = profesor.id_profesor GROUP BY persona.id;
-
--- 10
--- SELECT * FROM persona WHERE persona.tipo = 'alumno' AND YEAR(fecha_nacimiento) = (SELECT MIN(YEAR(fecha_nacimiento)) FROM persona WHERE persona.tipo = 'alumno');
-
--- 11
--- SELECT * FROM persona LEFT JOIN profesor ON profesor.id_profesor = persona.id LEFT JOIN departamento ON departamento.id = profesor.id_departamento LEFT JOIN asignatura ON asignatura.id_profesor = profesor.id_profesor WHERE asignatura.id IS NULL AND departamento.id IS NOT NULL;
-
+SELECT apellido1, apellido2, nombre FROM persona WHERE tipo = 'alumno' ORDER BY apellido1, apellido2, nombre ASC;
+SELECT nombre, apellido1, apellido2 FROM persona WHERE tipo = 'alumno' AND telefono IS NULL;
+SELECT * FROM persona WHERE tipo = 'alumno' AND YEAR(fecha_nacimiento) = '1999';
+SELECT * FROM persona WHERE tipo = 'profesor' AND telefono IS NULL AND nif LIKE '%K' ;
+SELECT * FROM asignatura WHERE cuatrimestre = 1 AND curso = 3 AND id_grado = 7;
+SELECT  persona.apellido1, persona.apellido2, persona.nombre, departamento.nombre FROM persona LEFT JOIN profesor ON persona.id = profesor.id_profesor INNER JOIN departamento ON departamento.id = profesor.id_departamento ORDER BY persona.apellido1, persona.apellido2, persona.nombre ASC;
+SELECT asignatura.nombre, curso_escolar.anyo_inicio, curso_escolar.anyo_fin FROM asignatura INNER JOIN alumno_se_matricula_asignatura ON alumno_se_matricula_asignatura.id_asignatura = asignatura.id INNER JOIN persona ON alumno_se_matricula_asignatura.id_alumno = persona.id INNER JOIN curso_escolar ON curso_escolar.id=alumno_se_matricula_asignatura.id_curso_escolar WHERE nif = '26902806M';
+SELECT DISTINCT departamento.nombre AS 'Departamento' FROM persona INNER JOIN profesor ON profesor.id_profesor = persona.id INNER JOIN departamento ON departamento.id = profesor.id_departamento INNER JOIN asignatura ON asignatura.id_profesor = profesor.id_profesor INNER JOIN grado ON grado.id = asignatura.id_grado WHERE persona.tipo = 'profesor' AND grado.nombre = 'Grado en Ingeniería Informática (Plan 2015)';
+SELECT DISTINCT persona.nombre, persona.apellido1, persona.apellido2 FROM persona LEFT JOIN alumno_se_matricula_asignatura ON persona.id = alumno_se_matricula_asignatura.id_alumno LEFT JOIN curso_escolar ON curso_escolar.id = alumno_se_matricula_asignatura.id_curso_escolar WHERE curso_escolar.anyo_inicio = '2018';
+SELECT departamento.nombre AS 'DEPARTAMENTO', persona.nombre, persona.apellido1, persona.apellido2 FROM persona LEFT JOIN profesor ON profesor.id_profesor = persona.id LEFT JOIN departamento ON departamento.id = profesor.id_departamento WHERE persona.tipo = 'profesor' ORDER BY departamento.nombre ASC, persona.apellido1 ASC, persona.apellido2 ASC, persona.nombre ASC;
+SELECT persona.nombre, persona.apellido1, persona.apellido2 FROM persona LEFT JOIN profesor ON profesor.id_profesor = persona.id LEFT JOIN departamento ON departamento.id = profesor.id_departamento WHERE profesor.id_departamento IS NULL AND persona.tipo = 'profesor';
+SELECT departamento.nombre AS 'DEPARTAMENTO' FROM departamento LEFT JOIN profesor ON departamento.id = profesor.id_departamento WHERE profesor.id_departamento IS NULL;
+SELECT * FROM persona LEFT JOIN profesor ON profesor.id_profesor = persona.id LEFT JOIN asignatura ON asignatura.id_profesor = profesor.id_profesor WHERE asignatura.id_profesor IS NULL AND persona.tipo = 'profesor';
+SELECT asignatura.nombre FROM asignatura LEFT JOIN profesor ON profesor.id_profesor = asignatura.id_profesor WHERE profesor.id_profesor IS NULL;
+SELECT DISTINCT departamento.nombre FROM departamento LEFT JOIN profesor ON profesor.id_departamento = departamento.id LEFT JOIN asignatura ON asignatura.id_profesor = profesor.id_profesor LEFT JOIN alumno_se_matricula_asignatura ON alumno_se_matricula_asignatura.id_asignatura = asignatura.id LEFT JOIN curso_escolar ON curso_escolar.id = alumno_se_matricula_asignatura.id_curso_escolar WHERE curso_escolar.anyo_inicio IS NULL;
+SELECT COUNT(persona.id) FROM persona WHERE persona.tipo = 'alumno';
+SELECT COUNT(persona.id) FROM persona WHERE persona.tipo = 'alumno' AND YEAR(persona.fecha_nacimiento)='1999';
+SELECT departamento.nombre, COUNT(profesor.id_profesor) FROM departamento INNER JOIN profesor ON profesor.id_departamento = departamento.id GROUP BY departamento.id	 ORDER BY COUNT(profesor.id_profesor) DESC;
+SELECT departamento.nombre, COUNT(profesor.id_profesor) FROM departamento LEFT JOIN profesor ON profesor.id_departamento = departamento.id GROUP BY departamento.id ORDER BY COUNT(profesor.id_profesor) ASC; 
+SELECT grado.nombre, COUNT(asignatura.id) FROM grado LEFT JOIN asignatura ON asignatura.id_grado = grado.id GROUP BY grado.id ORDER BY COUNT(asignatura.id) DESC;
+SELECT grado.nombre, COUNT(asignatura.id) FROM grado INNER JOIN asignatura ON asignatura.id_grado = grado.id  GROUP BY grado.id HAVING COUNT(asignatura.id)>40  ORDER BY COUNT(asignatura.id) DESC;
+SELECT grado.nombre, asignatura.tipo, COUNT(asignatura.creditos) FROM grado LEFT JOIN asignatura ON asignatura.id_grado = grado.id GROUP BY grado.id, asignatura.tipo;
+SELECT curso_escolar.anyo_inicio, COUNT(persona.id) FROM curso_escolar INNER JOIN alumno_se_matricula_asignatura ON alumno_se_matricula_asignatura.id_curso_escolar = curso_escolar.id INNER JOIN persona ON persona.id	= alumno_se_matricula_asignatura.id_alumno WHERE persona.tipo = 'alumno' GROUP BY curso_escolar.id;
+SELECT persona.id, persona.nombre, persona.apellido1, persona.apellido2, COUNT(asignatura.id) FROM persona INNER JOIN profesor ON profesor.id_profesor = persona.id INNER JOIN asignatura ON asignatura.id_profesor = profesor.id_profesor GROUP BY persona.id;
+SELECT * FROM persona WHERE persona.tipo = 'alumno' AND YEAR(fecha_nacimiento) = (SELECT MIN(YEAR(fecha_nacimiento)) FROM persona WHERE persona.tipo = 'alumno');
+SELECT * FROM persona LEFT JOIN profesor ON profesor.id_profesor = persona.id LEFT JOIN departamento ON departamento.id = profesor.id_departamento LEFT JOIN asignatura ON asignatura.id_profesor = profesor.id_profesor WHERE asignatura.id IS NULL AND departamento.id IS NOT NULL;
--- NODE/SPRINT2/SPRINT 2.4/queries_restaurant_francesc.js
+++ NODE/SPRINT2/SPRINT 2.4/queries_restaurant_francesc.js
@@ -1,100 +1,35 @@
 
-// Select the database to use.
 use('restaurant');
-
-//1
-// db.restaurants.find({});
-
-//2
-// db.restaurants.find({},{restaurant_id:1,name:1,borough:1,cuisine:1})
-
-//3
-// db.restaurants.find({},{restaurant_id:1,name:1,borough:1,cuisine:1,_id:0})
-
-//4
-// db.restaurants.find({},{restaurant_id:1,name:1,borough:1,address:{zipcode:1},_id:0})
-
-//5
-// db.restaurants.find({borough : "Bronx"})
-
-//6
-// db.restaurants.find({borough : "Bronx"}).limit(5)
-
-//7
-// db.restaurants.find({borough : "Bronx"}).skip(5).limit(5)
-
-//8
-// db.restaurants.find({ grades : { $elemMatch : {score : {$gt: 90} } }})
-
-//9
-// db.restaurants.find({grades : {$elemMatch: { score : {$gt:80,$lt:100}}}})
-
-//10
-// db.restaurants.find({"address.coord.1" : {$lt : -95.754168}})
-
-//11
-// db.restaurants.find({$and : [{cuisine: {$not :{$eq : "American"}}},{ grades : {$elemMatch : {score : {$gt : 70}}}},{ "address.coord.0": {$lt :-65.754168}}],})
-
-//12
-// db.restaurants.find({cuisine :{$not : {$eq : "American"}},grades : {$elemMatch : {score : {$gt : 70}}}, "address.coord.0" : {$lt : -65.754168}})
-
-//13
-// db.restaurants.find({cuisine : {$not : /American/}, grades : {$elemMatch : { grade : "A"}}, borough : {$not : /Brooklyn/}}).sort({cuisine:1})
-
-//14
-// db.restaurants.find({name : /^Wil/},{restaurant_id:1,name:1,borough:1,cuisine:1})
-
-//15
-// db.restaurants.find({name : /ces$/},{restaurant_id:1,name:1,borough:1,cuisine:1})
-
-//16
-// db.restaurants.find({name : /Reg/},{restaurant_id:1,name:1,borough:1,cuisine:1})
-
-//17
-// db.restaurants.find({borough: /Bronx/ , $or : [{cuisine : /American/},{ cuisine : /Chinese/}]})
-
-//18
-// db.restaurants.find({borough : {$in : ["Staten Island","Queens","Bronx","Brooklyn"]} },{restaurant_id:1,name:1,borough:1, cuisine:1})
-
-//19
-// db.restaurants.find({borough :{$not : {$in : ["Staten Island","Queens","Bronx","Brooklyn"]}}},{restaurant_id:1,name:1,borough:1,cuisine:1})
-
-//20
-// db.restaurants.find({grades : {$elemMatch : {score : {$lt : 10}}}},{restaurant_id:1,name:1,borough:1,cuisine:1})
-
-//21
-// db.restaurants.find({cuisine : /Seafood/ }, {restaurant_id:1, name:1, borough:1, cuisine:1 })
-
-//22
-// db.restaurants.find({grades : {$elemMatch : {grade : /A/, score : 11, date : ISODate("2014-08-11T00:00:00Z")}}},{restaurant_id:1, name:1,grades:1})
-
-//23
-// db.restaurants.find({"grades.1.grade": /A/, "grades.1.score" : 9, "grades.1.date" : ISODate("2014-08-11T00:00:00Z")}, {restaurant_id:1, name:1, grades:1})
-
-//24
-// db.restaurants.find({"address.coord.1": {$gt : 42, $lt : 52}},{restaurant_id:1, name:1, address:{coord:1}})
-
-//25
-// DBQuery.shellBatchSize = 100; //Per mostrar més resultats s'ha de canviar aquest paràmetre
-// db.restaurants.find({}).sort({name:1})
-
-//26
-// db.restaurants.find({}).sort({name:-1})
-
-//27
-// db.restaurants.find({}).sort({cuisine:1, borough:-1})
-
-//28 //si només es busca per un camp, s'ha de fer aixi, si busquem per tots els camps anidats s'han de col.locar en el mateix ordre
-// db.restaurants.find({"address.street" : {$not : /^$/ }}) 
-
-//29
-// db.restaurants.find({"address.coord.0" : { $type: "double" },"address.coord.1" : { $type: "double" }})
-
-//30
-// db.restaurants.find({grades : {$elemMatch : {score : {$mod : [7,0]} }}},{restaurant_id:1,name:1,grades:1})
-
-//31
-// db.restaurants.find({name:/mon/},{restaurant_id:1, name:1, borough: 1, address:{coord:1},cuisine:1,_id:0})
-
-//32
+db.restaurants.find({});
+db.restaurants.find({},{restaurant_id:1,name:1,borough:1,cuisine:1})
+db.restaurants.find({},{restaurant_id:1,name:1,borough:1,cuisine:1,_id:0})
+db.restaurants.find({},{restaurant_id:1,name:1,borough:1,address:{zipcode:1},_id:0})
+db.restaurants.find({borough : "Bronx"})
+db.restaurants.find({borough : "Bronx"}).limit(5)
+db.restaurants.find({borough : "Bronx"}).skip(5).limit(5)
+db.restaurants.find({ grades : { $elemMatch : {score : {$gt: 90} } }})
+db.restaurants.find({grades : {$elemMatch: { score : {$gt:80,$lt:100}}}})
+db.restaurants.find({"address.coord.1" : {$lt : -95.754168}})
+db.restaurants.find({$and : [{cuisine: {$not :{$eq : "American"}}},{ grades : {$elemMatch : {score : {$gt : 70}}}},{ "address.coord.0": {$lt :-65.754168}}],})
+db.restaurants.find({cuisine :{$not : {$eq : "American"}},grades : {$elemMatch : {score : {$gt : 70}}}, "address.coord.0" : {$lt : -65.754168}})
+db.restaurants.find({cuisine : {$not : /American/}, grades : {$elemMatch : { grade : "A"}}, borough : {$not : /Brooklyn/}}).sort({cuisine:1})
+db.restaurants.find({name : /^Wil/},{restaurant_id:1,name:1,borough:1,cuisine:1})
+db.restaurants.find({name : /ces$/},{restaurant_id:1,name:1,borough:1,cuisine:1})
+db.restaurants.find({name : /Reg/},{restaurant_id:1,name:1,borough:1,cuisine:1})
+db.restaurants.find({borough: /Bronx/ , $or : [{cuisine : /American/},{ cuisine : /Chinese/}]})
+db.restaurants.find({borough : {$in : ["Staten Island","Queens","Bronx","Brooklyn"]} },{restaurant_id:1,name:1,borough:1, cuisine:1})
+db.restaurants.find({borough :{$not : {$in : ["Staten Island","Queens","Bronx","Brooklyn"]}}},{restaurant_id:1,name:1,borough:1,cuisine:1})
+db.restaurants.find({grades : {$elemMatch : {score : {$lt : 10}}}},{restaurant_id:1,name:1,borough:1,cuisine:1})
+db.restaurants.find({cuisine : /Seafood/ }, {restaurant_id:1, name:1, borough:1, cuisine:1 })
+db.restaurants.find({grades : {$elemMatch : {grade : /A/, score : 11, date : ISODate("2014-08-11T00:00:00Z")}}},{restaurant_id:1, name:1,grades:1})
+db.restaurants.find({"grades.1.grade": /A/, "grades.1.score" : 9, "grades.1.date" : ISODate("2014-08-11T00:00:00Z")}, {restaurant_id:1, name:1, grades:1})
+db.restaurants.find({"address.coord.1": {$gt : 42, $lt : 52}},{restaurant_id:1, name:1, address:{coord:1}})
+DBQuery.shellBatchSize = 100; // Per mostrar més resultats s'ha de canviar aquest paràmetre
+db.restaurants.find({}).sort({name:1})
+db.restaurants.find({}).sort({name:-1})
+db.restaurants.find({}).sort({cuisine:1, borough:-1}) //si només es busca per un camp, s'ha de fer aixi, si busquem per tots els camps anidats s'han de col.locar en el mateix ordre
+db.restaurants.find({"address.street" : {$not : /^$/ }}) 
+db.restaurants.find({"address.coord.0" : { $type: "double" },"address.coord.1" : { $type: "double" }})
+db.restaurants.find({grades : {$elemMatch : {score : {$mod : [7,0]} }}},{restaurant_id:1,name:1,grades:1})
+db.restaurants.find({name:/mon/},{restaurant_id:1, name:1, borough: 1, address:{coord:1},cuisine:1,_id:0})
 db.restaurants.find({name:/^Mad/},{restaurant_id:1, name:1, borough: 1, address:{coord:1},cuisine:1,_id:0})
